5a1df4643feaa3aec3b608aaaab7f712812fde5e
nothingwastaken <82754118+nothingwastaken@users.noreply.github.com>
2022-12-29 17:56:42 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 song = "";
+LWx = 0;
+LWy = 0;
+RWx = 0;
+RWy = 0;
+
+
 
 function preload(){
     song = loadSound("music.mp3");
+
 }
 
 function setup(){
@@ -10,6 +17,13 @@ function setup(){
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+
+function modelLoaded(){
+    console.log("ready");
 }
 
 function draw(){
@@ -18,4 +32,23 @@ function draw(){
 
 function play(){
     song.play();
+    song.setVolume(0.3);
+    song.rate(1);
 }
+
+function gotPoses(results){
+    if (results.length > 0){
+        console.log(results);
+        LWx = results[0].pose.leftWrist.x;
+        LWy = results[0].pose.leftWrist.y;
+        console.log("Left wrist x = " + LWx + " Left wrist y = " + LWy);
+        RWx = results[0].pose.rightWrist.x;
+        RWy = results[0].pose.rightWrist.y;
+        console.log("Right wrist x = " + RWx + " Right wrist y = " + RWy);
+
+    }
+}
+
+
+
+
